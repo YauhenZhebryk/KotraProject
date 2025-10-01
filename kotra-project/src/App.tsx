83fbff60948ef87bba3d86aeae1afe0e00ec0e1f
Evez <foksogzhecka@gmail.com>
2025-10-02 00:01:56 +0200

    clearProject

---
+++ kotra-project/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <>
+			<div className='text-center mt-6 text-2xl'>Clear project Vite + React + TypeScript + Vitest + TailwindCSS + ESlint + Prettier + pnpm</div>
+    </>
+  )
+}
+
+export default App

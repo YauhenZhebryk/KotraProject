7c17224ac8d17a061a66128c106c21ad262b08e8
Evez <foksogzhecka@gmail.com>
2025-10-05 20:20:02 +0200

    font sizes fixed and learn more button created

--- kotra-project/src/components/HeaderComponent.tsx
+++ kotra-project/src/components/HeaderComponent.tsx
@@ -1,20 +1,20 @@
 import kotraLogo from '../assets/images/cortraBlackLogo.png'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function HeaderComponent() {
 	return (
 		<>
-			<header className='fixed top-0 left-0 right-0 z-50 w-full flex flex-col items-center bg-main-bg'>
+			<header className='sticky top-0 z-50 w-full flex flex-col items-center bg-main-bg '>
 				<Link to="/">
-					<img src={kotraLogo} alt="" className='h-32 m-auto mt-8' />
+					<img src={kotraLogo} alt="" className='h-32 m-auto mt-2' />
 				</Link>
-				<nav className='w-full border-0 border-b-2 border-t-2 border-main-text p-4' >
+				<nav className='w-full border-0 border-b-2 border-t-2 border-main-text p-4 pl-5 pr-5' >
 					<ul className='w-full flex flex-row justify-between text-main-text flex-wrap font-medium text-base'>
 						<li className='hover:text-main-orange transition-all'>
-							<Link to="/">Главная</Link>
+							<NavLink to="/" className={({ isActive }) => isActive ? 'underline' : ''} end>Главная</NavLink>
 						</li>
 						<li className='hover:text-main-orange transition-all'>
-							<Link to="/">Бронирование</Link>
+							<NavLink to="/reservation" className={({ isActive }) => isActive ? 'underline' : ''} end>Бронирование</NavLink>
 						</li>
 						<li className='hover:text-main-orange transition-all'>
 							<Link to="/">Отзывы</Link>
@@ -26,7 +26,6 @@ function HeaderComponent() {
 				</nav>
 			</header>
 
-			<div className='h-40' aria-hidden='true' />
 		</>
 	)
 }

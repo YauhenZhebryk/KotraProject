fd41fbc33a93e6784c164650c4906277754365bb
Aleh Pilets <pilets.oleg@gmail.com>
2025-10-09 12:18:18 +0200

    Update footer

---
+++ kotra-project/src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import InstaLogo from "../assets/images/insta.svg"
+import PhoneLogo from "../assets/images/phone.svg"
+import GeoLogo from "../assets/images/geo.svg"
+
+
+function Footer() {
+	  return (
+		
+    <div className="bg-footer-bg text-main-text p-5 space-y-4 mt-10 text-xl">
+
+      <h2 className="text-footer-text">Контакты</h2>
+
+      <address>
+
+        <ul className="space-y-2 not-italic">
+
+          <li className="flex items-center gap-2">
+            <img src={InstaLogo} alt="Instagram" className="w-10 h-10" />
+            <a href="https://instagram.com/usadba_kotra" target="_blank" rel="noopener noreferrer">
+              @usadba_kotra
+            </a>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <img src={PhoneLogo} alt="Телефон" className="w-10 h-10" />
+            <a href="tel:+375292718888">+375 (29) 271 88 88</a>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <img src={GeoLogo} alt="Адрес" className="w-10 h-10" />
+			<a href="https://www.google.com/maps/place/%D0%A3%D1%81%D0%B0%D0%B4%D1%8C%D0%B1%D0%B0+%D0%9A%D0%BE%D1%82%D1%80%D0%B0/@53.5977164,24.1887857,20.25z/data=!4m14!1m7!3m6!1s0x46dfcffe0beea665:0xb841cb550193f0b9!2z0KPRgdCw0LTRjNCx0LAg0JrQvtGC0YDQsA!8m2!3d53.5975719!4d24.1889852!16s%2Fg%2F11qqmhfs84!3m5!1s0x46dfcffe0beea665:0xb841cb550193f0b9!8m2!3d53.5975719!4d24.1889852!16s%2Fg%2F11qqmhfs84?entry=ttu&g_ep=EgoyMDI1MTAwMS4wIKXMDSoASAFQAw%3D%3D" 
+			target="blank" rel="noopener noreferrer">
+            Гродненский район, деревня Гущицы, Ул. Центральная 31а
+			</a>
+          </li>
+
+        </ul>
+      </address>
+    </div>
+  )
+}
+
+export default Footer
--- kotra-project/src/components/HomePage.tsx
+++ kotra-project/src/components/HomePage.tsx
@@ -5,6 +5,7 @@ import PlaceNavigation from './PlaceNavigation'
 import HeaderComponent from './HeaderComponent'
 import AllPlacesMainPage from './AllPlacesMainPage'
 import CommentSection from './CommentSection'
+import Footer from './Footer'
 
 
 function HomePage() {
@@ -16,6 +17,7 @@ function HomePage() {
 				<AllPlacesMainPage />
 				<CommentSection />
 			</div>
+			<Footer />
 		</>
 	)
 }

aae57fb8ee66a4b261c47312adce7c053d5964ed
YauhenZhebryk <foksogzhecka@gmail.com>
2025-11-13 10:44:13 +0100

    reservation page and form styled

--- kotra-project/src/components/InputComponent.tsx
+++ kotra-project/src/components/InputComponent.tsx
@@ -36,7 +36,14 @@ function InputComponent({ text, type = 'input', name, value, onChange, errorMess
                 />
             )}
 						{errorMessage ? (
-							<span className='red-500 p-2 border border-red-500 rounded-xl'>{errorMessage}</span>
+							<div className='flex flex-row'>
+								<div className='p-2 text-red-500'>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+									</svg>
+								</div>
+								<span>{errorMessage}</span>
+							</div>
 						) : <></>}
         </div>
     );

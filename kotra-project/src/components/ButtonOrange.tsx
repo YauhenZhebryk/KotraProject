fa4b35c09a6c7773858e9ac9fa461e4f91aa9e07
YauhenZhebryk <foksogzhecka@gmail.com>
2025-11-17 16:44:50 +0100

    buld the app

--- kotra-project/src/components/ButtonOrange.tsx
+++
@@ -1,9 +0,0 @@
-import {Button} from "@heroui/react";
-
-function ButtonOrange() {
-	return (
-		<Button className="bg-orange-500 hover:bg-orange-600 text-amber-100">Button</Button>
-	)
-}
-
-export default ButtonOrange
--- kotra-project/src/utils/theme.ts
+++
@@ -1,8 +0,0 @@
-export const getInitialTheme = (): boolean => {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme) {
-    return savedTheme === 'dark';
-  }
-  // Default to dark theme when no saved preference is found
-  return true;
-};
